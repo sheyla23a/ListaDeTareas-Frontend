b33eeea3f329490930b04ebe576c98b83f70b5ff
sheyla astorga <sheylaastorga1998@gmail.com>
2024-03-19 15:03:46 -0300

    modifique la funcion borrar tarea la cual no tomaba los ID de manera correcta para borrarlas

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,28 +1,63 @@
 import { Button } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
 import ListaTareas from "./ListaTareas";
-import { useState } from "react";
+import { useState,useEffect } from "react";
+import { eliminarTarea,crearTarea,obtenerTareas } from "../helpers/queries";
 const FormularioTarea = () => {
   const [tarea, setTarea] = useState("");
 
   const [tareas, setTareas] = useState([]);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    if (tarea.trim().length > 0){
-        setTareas([...tareas,{
-          "nombre" : tarea
-        }]);
-        setTarea('');
-    }else{
-        alert("Debes ingresar al menos una tarea")
+    if (tarea.trim().length >= 3 && tarea.trim().length <= 50){
+      const nuevaTarea={
+        "nombre" : tarea
+      };
+     try{
+      const respuesta = await crearTarea(nuevaTarea);
+        if (respuesta.status === 201) {
+          cargarTareas()
+          setTareaIngresada("");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      alert("Ingresa una tarea válida");
     }
   };
 
-  const borrarTarea = (tareaProps)=>{
-   const tareasFiltradas = tareas.filter((tarea)=>tarea !== tareaProps);
-   setTareas(tareasFiltradas);
+
+      let handleChange = (e) => {
+    setTareaIngresada(e.target.value);
+  };
+
+  const borrarTarea = async(tareaProps)=>{
+    try{
+      const respuesta = await eliminarTarea(tareaProps._id);
+      if (respuesta.status === 200){
+        const tareasFiltradas = tareas.filter((tarea)=>tarea._id !== tareaProps._id);
+        setTareas(tareasFiltradas);
+      }
+    }catch(error){
+     console.log(error)
+    }
   }
+ 
+  const cargarTareas = async () => {
+    try {
+      const respuesta = await obtenerTareas();
+      setTareas(respuesta);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    cargarTareas();
+  }, []);
+
 
   return (
     <section>
